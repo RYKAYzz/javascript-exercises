bc6f7115d1f946337bd1fd9724471ef8f1a26ec3
Henry Kay <mireyhen@gmail.com>
2025-06-14 08:43:11 +0300

    Complete first exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+function removeFromArray(arr, value) {
+  result = arr.slice();
+  return (results = result.filter((rem) => rem !== value));
+}
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

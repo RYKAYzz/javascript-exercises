4b375d1bdacaf73271fa8e31c031ade27760bab2
Henry Kay <mireyhen@gmail.com>
2025-06-14 09:03:33 +0300

    solve ignores non present values.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-function removeFromArray(arr, value) {
+function removeFromArray(arr, value1, value2) {
   result = arr.slice();
-  return (results = result.filter((rem) => rem !== value));
+  return (results = result.filter((rem) => rem !== value1 && rem !== value2));
 }
-console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 7, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

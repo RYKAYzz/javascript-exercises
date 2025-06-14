6ba5b3dbbcdcb65b817a77d18f51b09b2c11d887
Henry Kay <mireyhen@gmail.com>
2025-06-14 10:26:03 +0300

    Complete 09-sumAll exercise.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+function sumAll(firstInteger, secondInteger) {
+  let sum = 0;
+  if (!Number.isInteger(firstInteger) || !Number.isInteger(secondInteger)) {
+    return "ERROR";
+  }
 
-};
+  if (firstInteger < 1 || secondInteger < 1) {
+    return "ERROR";
+  }
+
+  if (firstInteger > secondInteger) {
+    [firstInteger, secondInteger] = [secondInteger, firstInteger];
+  }
+
+  for (let i = firstInteger; i <= secondInteger; i++) {
+    // declare num properly
+    sum += i;
+  }
+  return sum;
+}
+sumAll(-1, -21);
 
 // Do not edit below this line
 module.exports = sumAll;

d89fe6af3e3961604b6cffa855c3c78c9ae9734c
Henry Kay <mireyhen@gmail.com>
2025-06-14 09:23:43 +0300

    Complete exercise works with strings

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 function removeFromArray(arr, ...value) {
   return arr.filter((item) => !value.includes(item));
 }
-console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

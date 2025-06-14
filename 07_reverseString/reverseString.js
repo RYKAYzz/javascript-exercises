a84f7743058224136efa9c0a7472de8fcc8ef1db
Henry Kay <mireyhen@gmail.com>
2025-06-14 08:02:21 +0300

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
-
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+undefined;
+console.log(reverseString(""));
 // Do not edit below this line
 module.exports = reverseString;

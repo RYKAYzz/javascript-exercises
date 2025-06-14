be83bc18c0a4b88d7fac09af419ff9c63b18bd33
Henry Kay <mireyhen@gmail.com>
2025-06-14 09:17:54 +0300

    Complete test can remove all items by includes

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,7 @@
-function removeFromArray(arr, value1, value2) {
-  result = arr.slice();
-  return (results = result.filter((rem) => rem !== value1 && rem !== value2));
+function removeFromArray(arr, ...value) {
+  return arr.filter((item) => !value.includes(item));
 }
-console.log(removeFromArray([1, 2, 3, 4], 7, 2));
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;

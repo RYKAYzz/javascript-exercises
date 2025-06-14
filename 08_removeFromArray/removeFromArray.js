416e891c349d67f824ad0f6ea2fcc291070f8c47
Henry Kay <mireyhen@gmail.com>
2025-06-14 09:25:29 +0300

    Complete only removes same type exercise;

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 function removeFromArray(arr, ...value) {
   return arr.filter((item) => !value.includes(item));
 }
-console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3));
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

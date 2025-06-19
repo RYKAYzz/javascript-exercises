ce9f104a83926440b71a4ba86e43198ab9ff8a92
Henry Kay <mireyhen@gmail.com>
2025-06-19 23:36:30 +0300

    Complete exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -10,11 +10,21 @@ const sum = function (array) {
   return array.reduce((acc, tot) => acc + tot, 0);
 };
 
-const multiply = function () {};
+const multiply = function (array) {
+  return array.reduce((acc, tot) => acc * tot);
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b;
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+};
 
 // Do not edit below this line
 module.exports = {

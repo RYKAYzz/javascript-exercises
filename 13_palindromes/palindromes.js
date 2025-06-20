ff5b97544fd013180c688380dda50305e3b0f7ff
Henry Kay <mireyhen@gmail.com>
2025-06-20 10:52:35 +0300

    Complete exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const clean = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversed = clean.split("").reverse().join("");
+  if (clean === reversed) return true;
+  else return false;
 };
 
 // Do not edit below this line

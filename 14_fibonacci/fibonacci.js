45f061559c32dad249839adc33453d9303d6fc79
Henry Kay <mireyhen@gmail.com>
2025-06-20 12:12:08 +0300

    Complete exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number(n); //4
+  if (n < 0) return "OOPS"; //4<0
+  if (n === 0) return 0;
 
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= n; i++) {
+    const next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 };
 
 // Do not edit below this line

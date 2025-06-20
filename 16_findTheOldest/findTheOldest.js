bfe30d8b3f513937f5048160fd8bf5d5c31cd385
Henry Kay <mireyhen@gmail.com>
2025-06-20 11:52:36 +0300

    Complete exercise 16_findTheOldest.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
-
+function findTheOldest(people) {
+  return people.reduce((a, b) => {
+    const ageA = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    return ageB > ageA ? b : a;
+  });
+}
 // Do not edit below this line
 module.exports = findTheOldest;
